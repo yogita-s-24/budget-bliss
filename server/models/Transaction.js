33b213c1518d22ded691f8e74850733007560ed9
yogita-s-24 <yogitashete24@gmail.com>
2023-11-24 15:03:31 +0530

    feat : create Transaction schema in mongoose

---
+++ server/models/Transaction.js
@@ -0,0 +1,38 @@
+import { Schema, model } from "mongoose";
+
+const TransactionSchema = new Schema(
+  {
+    amount: {
+      type: Number,
+      required: true,
+    },
+
+    trasactionType: {
+      type: String,
+      enum: ["credit", "debit"],
+      required: true,
+    },
+
+    category: {
+      type: String,
+      enum: [
+        "food",
+        "education",
+        "entertainment",
+        "travel",
+        "shopping",
+        "other",
+      ],
+      default: "other",
+    },
+
+    description: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const Transaction = model("Transaction", TransactionSchema);
+
+export default Transaction;
