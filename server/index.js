5fa5f611ec60f8307c5b840af6a240b402e934c5
yogita-s-24 <yogitashete24@gmail.com>
2023-11-24 15:54:51 +0530

    feat(api) : creating post  enpoint for add transactions

--- server/index.js
+++ server/index.js
@@ -1,28 +1,56 @@
 import mongoose from "mongoose";
-import express, { json } from 'express';
-import dotenv from "dotenv"
+import express, { json } from "express";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const app =  express();
-app.use(express.json())
+import Transaction from "./models/Transaction.js";
 
-const connection = async() =>{
-    await mongoose.connect(process.env.MONGO_URI)
+const app = express();
+app.use(express.json());
+
+const connection = async () => {
+  await mongoose.connect(process.env.MONGO_URI);
 };
 
-if(connection){
-    console.log("MongoBD Connected Successfully");
+if (connection) {
+  console.log("MongoBD Connected Successfully");
 }
 connection();
 
-//health api foe testing 
-app.get('/api/health', async(req, res)=>{
-    res.send("Server is running");
-})
+app.post("/api/transactions", async (req, res) => {
+  const { amount, transactionType, category, description } = req.body;
+
+  const transaction = new Transaction({
+    amount,
+    transactionType,
+    category: category || "other",
+    description,
+  });
+
+  try {
+    const saveTransactionData = await transaction.save();
+
+    res.json({
+      success: true,
+      data: saveTransactionData,
+      message: "Transaction Save Successfully.",
+    });
+  } catch (err) {
+    res.json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+//health api foe testing
+app.get("/api/health", async (req, res) => {
+  res.send("Server is running");
+});
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
