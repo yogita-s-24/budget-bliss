26c330e59da26332bd8a944e4842ecd3df7696da
yogita-s-24 <yogitashete24@gmail.com>
2023-11-28 09:40:12 +0530

    feat: Add User schema with basic fields

---
+++ server/models/User.js
@@ -0,0 +1,35 @@
+import { Schema, model } from mongoose;
+
+const userSchema = new Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    bankName: {
+        type: String,
+        required: true,
+        enum: ["state-bank", "kotak-bank", "maharashtra-bank", "hdfc-bank", "axis-bank", "badoda-bank", "other"]
+    },
+    contactNumber: {
+        type: Number,
+        required: true
+    },
+    address: {
+        type: String,
+    }
+})
+
+const User = model("User", userSchema);
+export default User;
+
