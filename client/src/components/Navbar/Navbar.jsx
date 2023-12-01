0e1bf951650d3ff0838d78f89f62eb4d585b454e
yogita-s-24 <yogitashete24@gmail.com>
2023-12-01 11:39:04 +0530

    feat : implement user login and logout functionality in the navbar component

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,20 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import LoginImg from './switch.png';
 
 const Navbar = () => {
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    //get Loggedin user from local storage
+    const getLoggedInUserFromLocalStorage = JSON.parse(
+      localStorage.getItem("user" || "{}")
+    );
+    setUser(getLoggedInUserFromLocalStorage);
+    console.log(getLoggedInUserFromLocalStorage);
+  }, []);
 
   const handleMobileMenuToggle = () => {
     setMobileMenuOpen(!isMobileMenuOpen);
@@ -13,54 +25,131 @@ const Navbar = () => {
       {/* Navbar */}
       <nav className="bg-white shadow p-4">
         <div className="container mx-auto flex justify-between items-center ">
-
           {/* Logo */}
-          <div className="text-black hover:text-violet-600 text-xl font-bold flex-grow">Budget Bliss</div>
+          <div className="text-black hover:text-violet-600 text-xl font-bold flex-grow">
+            Budget Bliss
+          </div>
 
           {/* Navbar Links */}
           <div className="hidden md:flex items-center space-x-6 flex-grow">
-            <Link to="/" className="text-black hover:text-violet-600 ">Home</Link>
-            <Link to="/about" className="text-black hover:text-violet-600">About</Link>
-            <Link to="/addtransaction" className="text-black hover:text-violet-600">Add Transaction</Link>
-            <Link to="/showtransaction" className="text-black hover:text-violet-600">Show Transaction</Link>
-            <Link to="/signup" className="text-black hover:text-violet-600">Signup</Link>
-            <Link to="/login" className="text-black hover:text-violet-600">Login</Link>
-
-
+            <Link to="/" className="text-black hover:text-violet-600 ">
+              Home
+            </Link>
+            <Link to="/about" className="text-black hover:text-violet-600">
+              About
+            </Link>
+            <Link
+              to="/addtransaction"
+              className="text-black hover:text-violet-600">
+              Add Transaction
+            </Link>
+            <Link
+              to="/showtransaction"
+              className="text-black hover:text-violet-600">
+              Show Transaction
+            </Link>
+            <Link to="/signup" className="text-black hover:text-violet-600">
+              Signup
+            </Link>
+            <Link to="/login" className="text-black hover:text-violet-600">
+              Login
+            </Link>
           </div>
-          <div className=' hidden md:flex align-start'>
-          <Link to="/login" className=" px-5 py-1 font-bold hover:text-violet-600">User</Link>
-            
+
+          <div className=" hidden md:flex align-start text-black hover:text-violet-600 cursor-pointer">
+            Hello, {user?.userName || "User"}! {user?.userName ? (
+              <span
+                className="text-black hover:text-violet-600 cursor-pointer"
+                onClick={() => {
+                  localStorage.removeItem("user");
+                  window.location.href = "/login";
+                }}>
+                {" "}
+                <img src={LoginImg} alt="login-img" style={{height:"28px", marginLeft:"10px"}}/>
+              </span>
+            ) : null}
           </div>
-           
 
           {/* Mobile Navbar Toggle */}
           <div className="md:hidden">
-            <button onClick={handleMobileMenuToggle} className="text-black hover:text-violet-600">
-              <svg className="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+            <button
+              onClick={handleMobileMenuToggle}
+              className="text-black hover:text-violet-600">
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M4 6h16M4 12h16m-7 6h7"></path>
               </svg>
             </button>
           </div>
-
         </div>
       </nav>
 
       {/* Mobile Navbar (hidden by default) */}
-      <div className={`md:hidden ${isMobileMenuOpen ? '' : 'hidden'} bg-gray-100 p-4 text-black`}>
+      <div
+        className={`md:hidden ${
+          isMobileMenuOpen ? "" : "hidden"
+        } bg-gray-100 p-4 text-black`}>
         <ul className="list-none p-0 m-0">
-          <li><Link to="/" className="block py-2 hover:text-violet-600">Home</Link></li>
-          <li><Link to="/about" className="block py-2 hover:text-violet-600">About</Link></li>
-          <li><Link to="/addtransaction" className="block py-2 hover:text-violet-600">Add Transaction</Link></li>
-          <li><Link to="/showtransaction" className="block py-2 hover:text-violet-600">Show Transaction</Link></li>
-          <li><Link to="/signup" className="block py-2 hover:text-violet-600">Signup</Link></li>
-          <li><Link to="/login" className="block py-2 hover:text-violet-600">Login</Link></li>
-          <Link to="" className="text-black hover:text-violet-600">User</Link>
+          <li>
+            <Link to="/" className="block py-2 hover:text-violet-600">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="block py-2 hover:text-violet-600">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/addtransaction"
+              className="block py-2 hover:text-violet-600">
+              Add Transaction
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/showtransaction"
+              className="block py-2 hover:text-violet-600">
+              Show Transaction
+            </Link>
+          </li>
+          <li>
+            <Link to="/signup" className="block py-2 hover:text-violet-600">
+              Signup
+            </Link>
+          </li>
+          <li>
+            <Link to="/login" className="block py-2 hover:text-violet-600">
+              Login
+            </Link>
+          </li>
+
+
+          <div>
+            Hello, {user?.userName || " User "} !
+            {user?.userName ? (
+              <span
+                className="text-black hover:text-violet-600 cursor-pointer"
+                onClick={() => {
+                  localStorage.removeItem("user");
+                  window.location.href = "/login";
+                }}>
+                {" "}
+                <img src={LoginImg} alt="login-img" style={{height:"28px", marginLeft:"10px"}}/>
+              </span>
+            ) : null}
+          </div>
           
         </ul>
-      
       </div>
     </>
   );
