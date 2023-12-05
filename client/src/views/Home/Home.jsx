8c7c7c2bb76d5e83df564f14302a196c01b67a50
Yogita Shete <yogitashete24@gmail.com>
2023-12-05 15:46:50 +0530

    ref : styling added to home component

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import './Home.css'
 
 function Home() {
   return (
-    <div>
+    <div className="bg-img">
       <Navbar />
-      <h1 className="text-center mt-5">Budget Bliss!</h1>
+      <h1 className="text-center text-4xl mt-48 text-white font-serif">
+      <span className="text-violet-300 text-6xl">Budget Bliss </span>
+       <p className="mt-5 text-3xl">simplifies finances with easy tracking,</p> 
+       <p className="mt-5 text-2xl">budgeting, and insights.</p></h1>
     </div>
   );
 }
