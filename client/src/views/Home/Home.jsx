a1dfb19af194a987034173387ed98edc7a8bb209
Yogita Shete <yogitashete24@gmail.com>
2024-01-09 20:35:53 +0530

    ref : home page

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
     <div className="bg-img">
       <Navbar />
       <h1 className="text-center text-4xl mt-48 text-white font-serif">
-      <span className="text-violet-300 text-6xl">Budget Bliss </span>
+      <span className="text-violet-200 text-6xl">Budget Bliss </span>
        <p className="mt-5 text-3xl">simplifies finances with easy tracking,</p> 
        <p className="mt-5 text-2xl">budgeting, and insights.</p></h1>
     </div>
